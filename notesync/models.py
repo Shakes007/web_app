3341342763b9cd89ed972ec45cd2692351a15b8b
Samsodien <shaakirsamsodien@gmail.com>
2023-12-15 21:04:31 +0200

    add docstrings to models

--- notesync/models.py
+++ notesync/models.py
@@ -4,10 +4,25 @@ from django.db import models
 
 
 class Band(models.Model):
+    """
+    Model representing a band of music:
+
+    Attributes:
+        name (str): The name of the band.
+        genre (str): The type of music the band plays.
+    """
     name = models.CharField(max_length=150)
     genre = models.CharField(max_length=50)
 
 
 class Song(models.Model):
+    """
+    Model representing a class.
+
+    Attributes:
+        title (str): The name of the song.
+        band (Band): The band that performed the song.
+                     Linked as a foreign key.
+    """
     title = models.CharField(max_length=150)
     band = models.ForeignKey(Band, on_delete=models.CASCADE)
